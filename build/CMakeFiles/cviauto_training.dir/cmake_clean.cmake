2003b584b8d73375051438d76ea44464f19f0d99
tsj6666 <tsj15084739975@163.com>
2025-07-08 16:14:49 +0800

    satrt

---
+++ build/CMakeFiles/cviauto_training.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cviauto_training.dir/main.cpp.o"
+  "cviauto_training"
+  "cviauto_training.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cviauto_training.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tang/project/cviauto_training
+# Build directory: /home/tang/project/cviauto_training/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
